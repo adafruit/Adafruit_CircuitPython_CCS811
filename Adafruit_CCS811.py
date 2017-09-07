387bf909ca90b7f52d8701d1adb6daae5e8ee79c
Scott Shawcroft <scott@chickadee.tech>
2017-09-07 14:35:42 -0700

    Python-ify the API, setup docs and add the example.

--- Adafruit_CCS811.py
+++ Adafruit_CCS811.py
@@ -21,27 +21,17 @@
 # THE SOFTWARE.
 
 """
-`Adafruit_CCS811` - CCS811 air quality sensor
+`CCS811` - CCS811 air quality sensor
 ====================================================
-This library supports the use of the CCS811 in CircuitPython. This base
-class is inherited by the chip-specific subclasses.
-Functions are included for reading and writing registers and manipulating
-datetime objects.
-Author(s): Dean Miller for Adafruit Industries.
-Date: June 2017
-Affiliation: Adafruit Industries
-Implementation Notes
---------------------
-**Hardware:**
-*
-**Software and Dependencies:**
-* Adafruit CircuitPython firmware for the ESP8622 and M0-based boards: https://github.com/adafruit/micropython/releases
-* Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
-* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
-**Notes:**
-#. Datasheet: 
-"""
+This library supports the use of the CCS811 air quality sensor in CircuitPython.
 
+Author(s): Dean Miller for Adafruit Industries
+
+**Notes:**
+
+#. `Datasheet <https://cdn-learn.adafruit.com/assets/assets/000/044/636/original/CCS811_DS000459_2-00-1098798.pdf?1501602769>`_
+"""
+from micropython import const
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_register import i2c_bit
 from adafruit_register import i2c_bits
@@ -54,23 +44,19 @@ CCS811_ENV_DATA = const(0x05)
 CCS811_NTC = const(0x06)
 CCS811_THRESHOLDS = const(0x10)
 
-'''
-CCS811_BASELINE = 0x11
-CCS811_HW_ID = 0x20
-CCS811_HW_VERSION = 0x21
-CCS811_FW_BOOT_VERSION = 0x23
-CCS811_FW_APP_VERSION = 0x24
-CCS811_ERROR_ID = 0xE0
-'''
+# CCS811_BASELINE = 0x11
+# CCS811_HW_ID = 0x20
+# CCS811_HW_VERSION = 0x21
+# CCS811_FW_BOOT_VERSION = 0x23
+# CCS811_FW_APP_VERSION = 0x24
+# CCS811_ERROR_ID = 0xE0
 
 CCS811_SW_RESET = const(0xFF)
 
-'''
-CCS811_BOOTLOADER_APP_ERASE = 0xF1
-CCS811_BOOTLOADER_APP_DATA = 0xF2
-CCS811_BOOTLOADER_APP_VERIFY = 0xF3
-CCS811_BOOTLOADER_APP_START = 0xF4
-'''
+# CCS811_BOOTLOADER_APP_ERASE = 0xF1
+# CCS811_BOOTLOADER_APP_DATA = 0xF2
+# CCS811_BOOTLOADER_APP_VERIFY = 0xF3
+# CCS811_BOOTLOADER_APP_START = 0xF4
 
 CCS811_DRIVE_MODE_IDLE = const(0x00)
 CCS811_DRIVE_MODE_1SEC = const(0x01)
@@ -81,124 +67,106 @@ CCS811_DRIVE_MODE_250MS = const(0x04)
 CCS811_HW_ID_CODE = const(0x81)
 CCS811_REF_RESISTOR = const(100000)
 
-class Adafruit_CCS811:
+class CCS811:
+	"""CCS811 gas sensor driver.
+
+	:param ~busio.I2C i2c: The I2C bus.
+	:param int addr: The I2C address of the CCS811.
+	"""
 	#set up the registers
-	#self.status = Adafruit_bitfield([('ERROR' , 1), ('unused', 2), ('DATA_READY' , 1), ('APP_VALID', 1), ('unused2' , 2), ('FW_MODE' , 1)])
 	error = i2c_bit.ROBit(0x00, 0)
+	"""True when an error has occured."""
 	data_ready = i2c_bit.ROBit(0x00, 3)
+	"""True when new data has been read."""
 	app_valid = i2c_bit.ROBit(0x00, 4)
 	fw_mode = i2c_bit.ROBit(0x00, 7)
 
 	hw_id = i2c_bits.ROBits(8, 0x20, 0)
 
-	#self.meas_mode = Adafruit_bitfield([('unused', 2), ('INT_THRESH', 1), ('INT_DATARDY', 1), ('DRIVE_MODE', 3)])
 	int_thresh = i2c_bit.RWBit(0x01, 2)
 	interrupt_enabled = i2c_bit.RWBit(0x01, 3)
 	drive_mode = i2c_bits.RWBits(3, 0x01, 4)
 
-	#self.error_id = Adafruit_bitfield([('WRITE_REG_INVALID', 1), ('READ_REG_INVALID', 1), ('MEASMODE_INVALID', 1), ('MAX_RESISTANCE', 1), ('HEATER_FAULT', 1), ('HEATER_SUPPLY', 1)])
-
-	TVOC = 0
-	eCO2 = 0
-	
-	tempOffset = 0.0
+	temp_offset = 0.0
+	"""Temperature offset."""
 
 	def __init__(self, i2c, addr=0x5A):
 		self.i2c_device = I2CDevice(i2c, addr)
 
-			#check that the HW id is correct
+		#check that the HW id is correct
 		if self.hw_id != CCS811_HW_ID_CODE:
 			raise RuntimeError("Device ID returned is not correct! Please check your wiring.")
-		
+
 		#try to start the app
 		buf = bytearray(1)
 		buf[0] = 0xF4
-		self.i2c_device.write(buf, end=1, stop=True)
+		with self.i2c_device as i2c:
+			i2c.write(buf, end=1, stop=True)
 		time.sleep(.1)
-		
+
 		#make sure there are no errors and we have entered application mode
-		if self.checkError():
-			raise RuntimeError("Device returned an Error! Try removing and reapplying power to the device and running the code again.")
+		if self.error:
+			raise RuntimeError("Device returned a error! Try removing and reapplying power to the device and running the code again.")
 		if not self.fw_mode:
 			raise RuntimeError("Device did not enter application mode! If you got here, there may be a problem with the firmware on your sensor.")
-		
+
 		self.interrupt_enabled = False
-		
+
 		#default to read every second
-		self.setDriveMode(CCS811_DRIVE_MODE_1SEC)
+		self.drive_mode = CCS811_DRIVE_MODE_1SEC
 
+		self._eCO2 = None
+		self._TVOC = None
 
-	def setDriveMode(self, mode):
-		self.drive_mode = mode
+	@property
+	def error_code(self):
+		"""Error code"""
+		buf = bytearray(2)
+		buf[0] = 0xE0
+		with self.i2c_device as i2c:
+			i2c.write(buf, end=1, stop=False)
+			i2c.read_into(buf, start=1)
+		return buf[1]
 
-
-	def available(self):
-		return self.data_ready
-
-
-	def readData(self):
-
-		if not self.data_ready:
-			return False
-		else:
+	def _update_data(self):
+		if self.data_ready:
 			buf = bytearray(9)
 			buf[0] = CCS811_ALG_RESULT_DATA
-			self.i2c_device.write(buf, end=1, stop=False)
-			self.i2c_device.read_into(buf, start=1)
+			with self.i2c_device as i2c:
+				i2c.write(buf, end=1, stop=False)
+				i2c.read_into(buf, start=1)
 
-			self.eCO2 = (buf[1] << 8) | (buf[2])
-			self.TVOC = (buf[3] << 8) | (buf[4])
-			
+			self._eCO2 = (buf[1] << 8) | (buf[2])
+			self._TVOC = (buf[3] << 8) | (buf[4])
+
 			if self.error:
-				return buf[6]
-				
-			else:
-				return 0
-		
+				raise RuntimeError("Error:" + str(self.error_code))
 
+	@property
+	def TVOC(self):
+		"""Total Volatile Organic Compound in parts per billion."""
+		self._update_data()
+		return self._TVOC
 
-	def setEnvironmentalData(self, humidity, temperature):
+	@property
+	def eCO2(self):
+		"""Equivalent Carbon Dioxide in parts per million. Clipped to 400 to 8192ppm."""
+		return self._eCO2
 
-		''' Humidity is stored as an unsigned 16 bits in 1/512%RH. The
-		default value is 50% = 0x64, 0x00. As an example 48.5%
-		humidity would be 0x61, 0x00.'''
-		
-		''' Temperature is stored as an unsigned 16 bits integer in 1/512
-		degrees there is an offset: 0 maps to -25C. The default value is
-		25C = 0x64, 0x00. As an example 23.5% temperature would be
-		0x61, 0x00.
-		The internal algorithm uses these values (or default values if
-		not set by the application) to compensate for changes in
-		relative humidity and ambient temperature.'''
-		
-		hum_perc = humidity << 1
-		
-		parts = math.fmod(temperature)
-		fractional = parts[0]
-		temperature = parts[1]
-
-		temp_high = ((temperature + 25) << 9)
-		temp_low = ((fractional / 0.001953125) & 0x1FF)
-		
-		temp_conv = (temp_high | temp_low)
-
-		buf = bytearray([CCS811_ENV_DATA, hum_perc, 0x00,((temp_conv >> 8) & 0xFF), (temp_conv & 0xFF)])
-		
-		self.i2c_device.write(buf)
-
-
-
-	#calculate temperature based on the NTC register
-	def calculateTemperature(self):
+	@property
+	def temperature(self):
+		"""Temperature based on optional thermistor in Celsius."""
 		buf = bytearray(5)
 		buf[0] = CCS811_NTC
-		self.i2c_device.write(buf, end=1, stop=False)
-		self.i2c_device.read_into(buf, start=1)
+		with self.i2c_device as i2c:
+			i2c.write(buf, end=1, stop=False)
+			i2c.read_into(buf, start=1)
 
 		vref = (buf[1] << 8) | buf[2]
 		vntc = (buf[3] << 8) | buf[4]
 
-		#from ams ccs811 app note
+		# From ams ccs811 app note 000925
+		# https://download.ams.com/content/download/9059/13027/version/1/file/CCS811_Doc_cAppNote-Connecting-NTC-Thermistor_AN000372_v1..pdf
 		rntc = float(vntc) * CCS811_REF_RESISTOR / float(vref)
 
 		ntc_temp = math.log(rntc / 10000.0)
@@ -206,20 +174,50 @@ class Adafruit_CCS811:
 		ntc_temp += 1.0 / (25 + 273.15)
 		ntc_temp = 1.0 / ntc_temp
 		ntc_temp -= 273.15
-		return ntc_temp - self.tempOffset
+		return ntc_temp - self.temp_offset
 
+	def set_environmental_data(self, humidity, temperature):
+		"""Set the temperature and humidity used when computing eCO2 and TVOC values.
 
-	def setThresholds(self, low_med, med_high, hysteresis):
+		:param int humidity: The current relative humidity in percent.
+		:param float temperature: The current temperature in Celsius."""
+		# Humidity is stored as an unsigned 16 bits in 1/512%RH. The default
+		# value is 50% = 0x64, 0x00. As an example 48.5% humidity would be 0x61,
+		# 0x00.
+		hum_perc = int(humidity) << 1
+
+		# Temperature is stored as an unsigned 16 bits integer in 1/512 degrees
+		# there is an offset: 0 maps to -25C. The default value is 25C = 0x64,
+		# 0x00. As an example 23.5% temperature would be 0x61, 0x00.
+		parts = math.fmod(temperature)
+		fractional = parts[0]
+		temperature = parts[1]
+
+		temp_high = ((temperature + 25) << 9)
+		temp_low = ((fractional / 0.001953125) & 0x1FF)
+
+		temp_conv = (temp_high | temp_low)
+
+		buf = bytearray([CCS811_ENV_DATA, hum_perc, 0x00,((temp_conv >> 8) & 0xFF), (temp_conv & 0xFF)])
+
+		with self.i2c_device as i2c:
+			i2c.write(buf)
+
+	def set_interrupt_thresholds(self, low_med, med_high, hysteresis):
+		"""Set the thresholds used for triggering the interrupt based on eCO2.
+		The interrupt is triggered when the value crossed a boundary value by the
+		minimum hysteresis value.
+
+		:param int low_med: Boundary between low and medium ranges
+		:param int med_high: Boundary between medium and high ranges
+		:param int hysteresis: Minimum difference between reads"""
 		buf = bytearray([CCS811_THRESHOLDS, ((low_med >> 8) & 0xF), (low_med & 0xF), ((med_high >> 8) & 0xF), (med_high & 0xF), hysteresis ])
-		self.i2c_device.write(buf)
+		with self.i2c_device as i2c:
+			self.i2c_device.write(buf)
 
-
-	def SWReset(self):
-
+	def reset(self):
+		"""Initiate a software reset."""
 		#reset sequence from the datasheet
 		seq = bytearray([CCS811_SW_RESET, 0x11, 0xE5, 0x72, 0x8A])
-		self.i2c_device.write(seq)
-
-
-	def checkError(self):
-		return self.error
+		with self.i2c_device as i2c:
+			self.i2c_device.write(seq)
---
+++ api.rst
@@ -0,0 +1,3 @@
+
+.. automodule:: adafruit_ccs811
+   :members:
