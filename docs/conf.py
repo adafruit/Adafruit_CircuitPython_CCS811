4b03fe829b1b0664e8a1c1cf4223b17e5fa34dd6
sommersoft <sommersoft@gmail.com>
2018-03-10 09:32:25 -0600

    updated conf.py; bus device intersphinx link

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,8 @@ extensions = [
 # autodoc module docs will fail to generate with a warning.
 autodoc_mock_imports = ["micropython", "adafruit_bus_device", "adafruit_register"]
 
+intersphinx_mapping = {'python': ('https://docs.python.org/3.4', None),'BusDevice': ('https://circuitpython.readthedocs.io/projects/busdevice/en/latest/', None),'CircuitPython': ('https://circuitpython.readthedocs.io/en/latest/', None)}
+
 # Mock out micropython ourselves.
 #import imp
 #m = imp.new_module("micropython")
