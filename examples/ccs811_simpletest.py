e82148976bbf5fb36d07698b0965bd25aa2a25ee
Kattni Rembor <kattni@adafruit.com>
2018-08-15 14:53:38 -0400

    Refactor i2c and print statement

--- examples/ccs811_simpletest.py
+++ examples/ccs811_simpletest.py
@@ -3,8 +3,8 @@ import board
 import busio
 import adafruit_ccs811
 
-i2c_bus = busio.I2C(board.SCL, board.SDA)
-ccs811 = adafruit_ccs811.CCS811(i2c_bus)
+i2c = busio.I2C(board.SCL, board.SDA)
+ccs811 = adafruit_ccs811.CCS811(i2c)
 
 # Wait for the sensor to be ready and calibrate the thermistor
 while not ccs811.data_ready:
@@ -13,6 +13,6 @@ temp = ccs811.temperature
 ccs811.temp_offset = temp - 25.0
 
 while True:
-    print("CO2: %1.0f PPM, TVOC: %1.0f PPM, Temp: %0.1f C" %
-          (ccs811.eco2, ccs811.tvoc, ccs811.temperature))
+    print("CO2: {} PPM, TVOC: {} PPM, Temp: {} C"
+          .format(ccs811.eco2, ccs811.tvoc, ccs811.temperature))
     time.sleep(0.5)
