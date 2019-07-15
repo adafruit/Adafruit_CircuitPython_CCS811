ee5774ddeede22ed9a7dc904322544de27e55901
caternuson <caternuson@gmail.com>
2019-07-15 15:15:30 -0700

    deprecate temperature

--- examples/ccs811_simpletest.py
+++ examples/ccs811_simpletest.py
@@ -6,13 +6,11 @@ import adafruit_ccs811
 i2c = busio.I2C(board.SCL, board.SDA)
 ccs811 = adafruit_ccs811.CCS811(i2c)
 
-# Wait for the sensor to be ready and calibrate the thermistor
+# Wait for the sensor to be ready
 while not ccs811.data_ready:
     pass
-temp = ccs811.temperature
-ccs811.temp_offset = temp - 25.0
 
 while True:
-    print("CO2: {} PPM, TVOC: {} PPM, Temp: {} C"
-          .format(ccs811.eco2, ccs811.tvoc, ccs811.temperature))
+    print("CO2: {} PPM, TVOC: {} PPM"
+          .format(ccs811.eco2, ccs811.tvoc)
     time.sleep(0.5)
